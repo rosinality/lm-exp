7aa84885401c88bf1bfb75c5e7fdc53c818fdc1c
matt.mldev (Kim Seonghyeon) <matt.mldev@kakaobrain.com>
2024-05-21 21:57:04 +0900

    Test mixture

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
 
-sudo NEEDRESTART_MODE=a apt update -y
-sudo NEEDRESTART_MODE=a apt install -y python3.10-venv gcsfuse
+sudo NEEDRESTART_MODE=a apt-get update -y
+sudo NEEDRESTART_MODE=a apt-get install -y python3.10-venv gcsfuse
 
 python3.10 -m venv openmoe_venv
 
